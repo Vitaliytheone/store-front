3292065c57c36c29e9154b6bafbd80562444ff7e
Alex Fatyeev <alex.fatyeev@gmail.com>
2017-11-17 09:31:09 +0300

    [ messages ]

---
+++ messages/en/admin.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Translation map for en-US
+ */
+return [
+
+    'orders.page_title' => 'Orders',
+    'orders.page_name' => 'Orders',
+
+    'orders.search_placeholder' => 'Search for...',
+
+    'orders.filter_all' => 'All orders',
+    'orders.filter_status_awaiting' => 'Awaiting',
+    'orders.filter_status_pending' => 'Pending',
+    'orders.filter_status_in_progress' => 'In progress',
+    'orders.filter_status_completed' => 'Conmpeted',
+    'orders.filter_status_canceled' => 'Canceled',
+    'orders.filter_status_failed' => 'Failed',
+    'orders.filter_status_error' => 'Error',
+
+    'orders.filter_product_all' => 'All',
+
+    'orders.filter_mode_all' => 'All',
+    'orders.filter_mode_manual' => 'Manual',
+    'orders.filter_mode_auto' => 'Auto',
+
+    'orders.action_title' => 'Actions',
+    'orders.action_change_status' => 'Change status',
+    'orders.action_resend' => 'Resend order',
+    'orders.action_cancel' => 'Cancel',
+    'orders.action_details' => 'Details',
+
+    'orders.window_details_title' => 'Order suborder_id} details',
+    'orders.window_details_provider' => 'Provider',
+    'orders.window_details_order_id' => 'Provider\'s order ID',
+    'orders.window_details_response' => 'Provider\'s response',
+    'orders.window_details_last_update' => 'Last update',
+
+
+    'payments.page_title' => 'Payments',
+    'payments.page_name' => 'Payments',
+
+    'payments.search_placeholder' => 'Search for...',
+
+    'payments.filter_all' => 'All orders',
+    'payments.filter_status_awaiting' => 'Awaiting',
+    'payments.filter_status_completed' => 'Completed',
+    'payments.filter_status_failed' => 'Failed',
+    'payments.filter_status_refunded' => 'Refunded',
+
+    'payments.filter_method_all' => 'All',
+
+    'payments.action_title' => 'Actions',
+    'payments.action_details' => 'Details',
+
+    'payments.window_details_title' => 'Payment payment_id} details',
+
+
+    'products.page_title' => 'Products',
+    'products.page_name' => 'Products',
+
+    'products.button_add_product_title' => 'Add product',
+    'products.button_edit_product_title' => 'Edit',
+    'products.button_add_package_title' => 'Add package',
+    'products.button_edit_package_title' => 'Edit',
+    'products.button_delete_package_title' => 'Delete',
+
+    'products.window_product_create_title' => 'Create product',
+    'products.window_product_update_title' => 'Update product',
+    'products.window_product_create_button_save_title' => 'Add product',
+    'products.window_product_update_button_save_title' => 'Save product',
+    'products.window_product_button_cancel_title' => 'Cancel',
+    'products.window_product_p_name' => 'Product name',
+    'products.window_product_p_visibility' => 'Visibility',
+    'products.window_product_p_visibility_option_enabled' => 'Enabled',
+    'products.window_product_p_visibility_option_disabled' => 'Disabled',
+    'products.window_product_p_properties_title' => 'Properties',
+    'products.window_product_p_properties_placeholder' => 'Add property',
+    'products.window_product_p_properties_button_add' => 'Add',
+    'products.window_product_seo_button_edit_title' => 'Edit website SEO',
+    'products.window_product_seo_preview_title' => 'Search engine listing preview',
+    'products.window_product_seo_page_title' => 'Page title',
+    'products.window_product_seo_page_title_chars_used' => '{current} of {limit} characters used',
+    'products.window_product_seo_meta' => 'Meta description',
+    'products.window_product_seo_meta_chars_used' => '{current} of {limit} characters used',
+    'products.window_product_seo_url' => 'URL',
+
+    'products.window_package_create_title' => 'Create package',
+    'products.window_package_update_title' => 'Update package',
+    'products.window_package_create_button_save_title' => 'Add package',
+    'products.window_package_update_button_save_title' => 'Save package',
+    'products.window_package_button_cancel_title' => 'Cancel',
+    'products.window_package_p_name' => 'Package name',
+    'products.window_package_p_price' => 'Price',
+    'products.window_package_p_quantity' => 'Quantity',
+    'products.window_package_p_best' => 'Best package',
+    'products.window_package_p_best_option_enabled' => 'Enabled',
+    'products.window_package_p_best_option_disabled' => 'Disabled',
+    'products.window_package_p_link' => 'Link Type',
+    'products.window_package_p_link_option_default' => 'None',
+    'products.window_package_p_availability' => 'Availability',
+    'products.window_package_p_availability_option_enabled' => 'Enabled',
+    'products.window_package_p_availability_option_disabled' => 'Disabled',
+    'products.window_package_p_mode' => 'Mode',
+    'products.window_package_p_mode_option_manual' => 'Manual',
+    'products.window_package_p_mode_option_auto' => 'Auto',
+    'products.window_package_p_provider' => 'Provider',
+    'products.window_package_p_provider_option_default' => 'Chose providers',
+    'products.window_package_p_service' => 'Provider service',
+    'products.window_package_p_service_option_default' => 'Chose service',
+
+
+    'settings.page_title' => 'Settings',
+    'settings.page_name' => 'Settings',
+
+    'settings.left_menu_general_title' => 'General',
+    'settings.left_menu_payments_title' => 'Payments',
+    'settings.left_menu_providers_title' => 'Providers',
+    'settings.left_menu_navigation_title' => 'Navigation',
+    'settings.left_menu_pages_title' => 'Pages',
+    'settings.left_menu_blocks_title' => 'Blocks',
+    'settings.section_general_title' => 'General',
+    'settings.section_general_logo_title' => 'Logo',
+    'settings.section_general_logo_button_upload_title' => 'Upload logo',
+    'settings.section_general_logo_text_limits' => 'Image available types: jpg, png or gif. Maximum image size 3 Mb.',
+    'settings.section_general_favicon_title' => 'Favicon',
+    'settings.section_general_favicon_button_upload_title' => 'Upload favicon',
+    'settings.section_general_favicon_text_limits' => 'Image available types: jpg, png, gif or ico. Maximum image size 0.5 Mb',
+    'settings.section_general_store_name_title' => 'Store name',
+    'settings.section_general_store_input_placeholder' => 'Input store name',
+    'settings.section_general_timezone_title' => 'Timezone',
+    'settings.section_general_seo_title' => 'Search engine listing preview',
+    'settings.section_general_seo_button_edit_title' => 'Edit website SEO',
+    'settings.section_general_seo_index_title' => 'Title index',
+    'settings.section_general_seo_index_title_input_default' => 'About Us',
+    'settings.section_general_seo_index_title_input_limits' => '{current} of {limit} characters used',
+    'settings.section_general_seo_meta_title' => 'Meta description',
+    'settings.section_general_seo_meta_title_input_default' => 'A great About Us page helps builds trust between you and your customers. The more content you provide about you and your business, the more confident people will text',
+    'settings.section_general_seo_meta_title_input_limits' => '{current} of {limit} characters used',
+    'settings.button_save_title' => 'Save changes',
+
+
+
+];
