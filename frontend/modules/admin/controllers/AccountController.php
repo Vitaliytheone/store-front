ced275006947545389fec194de3ed8bc0a97f89a
Alex Fatyeev <alex.fatyeev@gmail.com>
2018-01-16 15:36:23 +0300

    [ only for create new user ]

---
+++ frontend/modules/admin/controllers/AccountController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace frontend\modules\admin\controllers;
+
+use common\models\stores\StoreAdmins;
+use frontend\modules\admin\components\Url;
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+/**
+ * Site controller for the `admin` module
+ */
+class AccountController extends CustomController
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['index'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ]
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * Renders account management page
+     * @return string
+     */
+    public function actionIndex()
+    {
+
+    }
+
+
+    // TODO:: REMOVE THIS ACTION!!!
+    public function actionAddAdmin()
+    {
+        $user = StoreAdmins::find()->where(['username' => 'admin'])->one();
+        if (empty($user)) {
+            $user = new StoreAdmins();
+            $user->store_id = Yii::$app->store->getInstance()->id;
+            $user->username = 'admin';
+            $user->status = true;
+            $user->setPassword('admin');
+            $user->generateAuthKey();
+            if ($user->save()) {
+                error_log(print_r($user->attributes, 1), 0);
+            }
+        }
+    }
+}
