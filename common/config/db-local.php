90aa189e5b69099c1e6a10896966e7bbc6b9faf1
Alex Fatyeev <alex.fatyeev@gmail.com>
2018-02-20 14:45:29 +0300

    [ Common config ]: 1. DB access configs moved to separated files: db.php, db-local.php. 2. Small changes in db init on StoreComponent.

---
+++ common/config/db-local.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'username' => 'sommerce_v1_dev',
+        'password' => '2211',
+    ],
+    'store_db' => [
+        'host' => '127.0.0.1',
+        'username' => 'sommerce_v1_dev',
+        'password' => '2211',
+    ],
+];
