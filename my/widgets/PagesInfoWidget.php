75f522f4bbeac9ef3568f0988f25de25dba2ddd4
Alex Fatyeev <alex.fatyeev@gmail.com>
2018-03-16 14:46:44 +0300

    My widgets moved

---
+++ my/widgets/PagesInfoWidget.php
@@ -0,0 +1,45 @@
+<?php
+namespace my\widgets;
+
+use yii\base\InvalidParamException;
+use yii\base\Widget;
+use yii\data\Pagination;
+
+/**
+ * Class PagesInfoWidget
+ * @package my\widgets
+ */
+class PagesInfoWidget extends Widget {
+
+    /**
+     * @var Pagination
+     */
+    public $pagination;
+
+    /**
+     * Run method
+     * @return string|void
+     */
+    public function run()
+    {
+        if (!($this->pagination instanceof Pagination)) {
+            throw new InvalidParamException();
+        }
+
+        $from = $this->pagination->offset + 1;
+
+        $total = $this->pagination->totalCount;
+
+        $to = $this->pagination->offset + $this->pagination->limit;
+
+        if ($total < $to) {
+            $to = $total;
+        }
+
+        if (!$total) {
+            return;
+        }
+
+        echo $from . ' to ' . $to . ' of ' . $total;
+    }
+}
