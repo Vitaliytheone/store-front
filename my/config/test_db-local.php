52333423d6f12f7c2d48a357e2289762689848d4
vitaliysh <shulyakwork@gmail.com>
2018-03-19 10:01:54 +0300

    #ID-215 - Removed local config files and created runtime folder

--- my/config/test_db-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=panels_test',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
