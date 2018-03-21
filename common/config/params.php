e4c220b25f8ff37d9475d28b51b32bdd4f5bcc23
vitaliysh <shulyakwork@gmail.com>
2018-03-21 16:51:51 +0300

    #ID-215 Some fixes with my and config.json

--- common/config/params.php
+++ common/config/params.php
@@ -1,21 +1,22 @@
 <?php
 
 $configPath = __DIR__ . '/config.json';
+$configPath = file_exists($configPath) ? $configPath : '/var/www/cfg/config.json';
 
 $configParams = (function() use ($configPath) {
-    $configParams = file_get_contents($configPath);
+    $configParams = file_exists($configPath) ? file_get_contents($configPath) : [];
     return !empty($configParams) ? json_decode($configParams, true) : [];
 })();
 
 $dbParams = (function() use ($configParams) {
-    if (!$configParams['db']) {
+    if (empty($configParams['db'])) {
         throw new Exception('DB is not configured yet!');
     }
     return $configParams['db'];
 })();
 
 $proxyParams = (function() use ($configParams) {
-    if (!$configParams['proxy']) {
+    if (empty($configParams['proxy'])) {
         throw new Exception('Proxy is not configured yet!');
     }
     return [
