dec7fd5366198e134c5436f765bcdd5436a814cf
Alex Fatyeev <alex.fatyeev@gmail.com>
2018-03-22 17:23:18 +0300

    [ DB : migrations ] DB stores, DBS store_ converted to `utf8mb4_unicode_ci`.

---
+++ common/migrations/sommerce/m180322_124242_stores_database_collation_conversions.php
@@ -0,0 +1,57 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180322_124242_stores_database_collation_conversions
+ */
+class m180322_124242_stores_database_collation_conversions extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180322_124242_stores_database_collation_conversions cannot be reverted.\n";
+
+        return false;
+    }
+
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        $this->execute('
+            ALTER DATABASE `stores` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;       
+        ');
+
+        $this->execute('
+            USE `stores`;
+            ALTER TABLE customers CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            ALTER TABLE default_themes CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            ALTER TABLE payment_methods CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;  
+            ALTER TABLE providers CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            ALTER TABLE store_admins CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            ALTER TABLE store_admins_hash CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;    
+            ALTER TABLE store_domains CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            ALTER TABLE store_providers CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            ALTER TABLE stores CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            ALTER TABLE stores_send_orders CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+            ALTER TABLE system_migrations CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+        ');
+    }
+
+    public function down()
+    {
+        echo "m180322_124242_stores_database_collation_conversions cannot be reverted.\n";
+
+        return false;
+    }
+}
