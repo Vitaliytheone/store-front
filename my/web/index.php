022f56a3d208e3bd1f97f53b170eabfa883934ef
vitaliysh <shulyakwork@gmail.com>
2018-03-29 18:12:56 +0300

    Updated environments

--- my/web/index.php
+++ my/web/index.php
@@ -1,7 +1,9 @@
 <?php
-
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+if (file_exists(__DIR__ . '/../config/environments-local.php')) {
+    require(__DIR__ . '/../config/environments-local.php');
+} else {
+    require(__DIR__ . '/../config/environments.php');
+}
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
