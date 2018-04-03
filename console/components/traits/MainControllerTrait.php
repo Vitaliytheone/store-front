26675156b5c65bb2d791ba4a4b6c22c3b9a394ef
vitaliysh <shulyakwork@gmail.com>
2018-04-03 15:51:56 +0300

    Fix with console command

--- console/components/traits/MainControllerTrait.php
+++ console/components/traits/MainControllerTrait.php
@@ -21,18 +21,18 @@ trait MainControllerTrait {
 
         $params = array_merge(
             require($commonPath . '/params.php'),
-            file_exists($commonPath . '/params-local.php') ? require($commonPath . '/params-local.php') : [],
+            file_exists($commonPath . '/params-local.php') ? (array)(require($commonPath . '/params-local.php')) : [],
             require($consolePath . '/params.php'),
-            file_exists($consolePath . '/params-local.php') ? require($consolePath . '/params-local.php') : [],
+            file_exists($consolePath . '/params-local.php') ? (array)(require($consolePath . '/params-local.php')) : [],
             require($frontendPath . '/params.php'),
-            file_exists($frontendPath . '/params-local.php') ? require($frontendPath . '/params-local.php') : []
+            file_exists($frontendPath . '/params-local.php') ? (array)(require($frontendPath . '/params-local.php')) : []
         );
 
         Yii::$app->params = $params;
 
         $db = array_merge(
             require($frontendPath. '/db.php'),
-            file_exists($frontendPath . '/db-local.php') ? require($frontendPath . '/db-local.php') : []
+            file_exists($frontendPath . '/db-local.php') ? (array)(require($frontendPath . '/db-local.php')) : []
         );
 
         foreach ($db as $name => $options) {
