4f8dd35c48bda3aec50745a47140b33a69e63420
Alex Fatyeev <alex.fatyeev@gmail.com>
2018-04-13 16:32:54 +0300

    Hot fixes.

--- common/migrations/my/m180413_064856_panels__notification_email_new_mssage_added.php
+++ common/migrations/my/m180413_064856_panels__notification_email_new_mssage_added.php
@@ -30,8 +30,7 @@ class m180413_064856_panels__notification_email_new_mssage_added extends Migrati
         $this->execute('
             USE `panels`;
             INSERT INTO `notification_email` (`subject`, `message`, `code`, `enabled`)
-            VALUES
-              (\'Paypal payment verification required\', \'To confirm the authenticity of your billing account, please click on the link below. \n{{verify_link}}\', \'paypal_verify\', 1);
+            VALUES (\'Paypal payment verification required\', \'To confirm the authenticity of your billing account, please click on the link below. \n \{\{verify_link\}\}\', \'paypal_verify\', 1);
         ');
     }
 
