074884d96f0e622e45963e384ff622e67d49429f
Alex Fatyeev <alex.fatyeev@gmail.com>
2018-04-16 14:57:33 +0300

    Fixes.

--- my/models/forms/SignupForm.php
+++ my/models/forms/SignupForm.php
@@ -71,7 +71,7 @@ class SignupForm extends Model
         $model->password_confirm = hash_hmac('sha256', $this->password_confirm, Yii::$app->params['auth_key']);
         $model->generateToken();
 
-        $model->stores = Customers::BUY_DOMAIN_ACTIVE;
+        $model->buy_domain = Customers::BUY_DOMAIN_ACTIVE;
 
         if (ReferralHelper::has()) {
             $code = ReferralHelper::get();
