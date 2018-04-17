8b711debc24a01f2c6459a873aa01206f56613d3
vitaliysh <shulyakwork@gmail.com>
2018-04-17 16:40:52 +0300

    #ID-331 Rename themes folders

--- common/migrations/sommerce/20180417_update_themes_data.sql
+++ common/migrations/sommerce/20180417_update_themes_data.sql
@@ -5,3 +5,7 @@ WHERE `folder` = 'smm24';
 UPDATE `default_themes` SET
 `name` = 'Bootstrap'
 WHERE `folder` = 'classic';
+
+UPDATE `default_themes` SET
+`folder` = 'bootstrap'
+WHERE `folder` = 'classic';
