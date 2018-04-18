5eb4560e3c5aa5a06ea3a6b4f17969b003aa2077
Alex Fatyeev <alex.fatyeev@gmail.com>
2018-04-18 12:21:31 +0300

    Hot fixes.

--- sommerce/config/params.php
+++ sommerce/config/params.php
@@ -9,7 +9,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'debugIps' => ['*'],
 
-    'auth_key' => '001100110011001100110011',
+    'auth_key' => '+^e91s&qm&9*hs9_z=1e8jq8rl@njmr#=ts16!f_23wo7-@s(r', // Must be a same as my/config/params.php 'auth_key'!
 
     'reCaptcha.siteKey' => '6LeAmT4UAAAAAKz1c-wjHdI2XDp_PglfA1rl8RbG',
     'reCaptcha.secret' => '6LeAmT4UAAAAAI88eDxVJkusrAurV9A8EqwphqVw',
