c0246b8b9a2cedd4a22127b7ecf0f8fdd19d8af5
Alex Fatyeev <alex.fatyeev@gmail.com>
2018-05-11 17:08:55 +0300

    #ID-431 Hot fixes: my/superadmin/invoices. Domains for ssl invoices added.

--- my/modules/superadmin/models/search/InvoicesSearch.php
+++ my/modules/superadmin/models/search/InvoicesSearch.php
@@ -89,6 +89,7 @@ class InvoicesSearch extends Invoices {
                 InvoiceDetails::ITEM_BUY_CHILD_PANEL,
                 InvoiceDetails::ITEM_BUY_STORE,
                 InvoiceDetails::ITEM_BUY_TRIAL_STORE,
+                InvoiceDetails::ITEM_PROLONGATION_SSL,
             ]) . ')'
         );
         $query->leftJoin(DB_PANELS . '.project', 'project.id = invoice_details.item_id AND invoice_details.item IN (' . implode(",", [
@@ -99,7 +100,6 @@ class InvoicesSearch extends Invoices {
         $query->leftJoin(DB_STORES . '.stores', 'stores.id = invoice_details.item_id AND invoice_details.item IN (' . implode(",", [
                 InvoiceDetails::ITEM_PROLONGATION_STORE,
         ]) . ')');
-        $query->leftJoin(DB_PANELS . '.ssl_cert', 'ssl_cert.id = invoice_details.item_id AND invoice_details.item = ' . InvoiceDetails::ITEM_PROLONGATION_SSL);
         $query->leftJoin(DB_PANELS . '.customers', 'customers.id = invoice_details.item_id AND invoice_details.item = ' . InvoiceDetails::ITEM_CUSTOM_CUSTOMER);
 
         return $query;
@@ -127,7 +127,7 @@ class InvoicesSearch extends Invoices {
 
         $invoices = $query->select([
                 'invoices.*',
-                'COALESCE(orders.domain, project.site, ssl_cert.domain, stores.domain, customers.email) as domain',
+                'COALESCE(orders.domain, project.site, stores.domain, customers.email) as domain',
                 'IF (invoice_details.item = ' . InvoiceDetails::ITEM_PROLONGATION_PANEL . ', 1, 0) as editTotal'
             ])->offset($pages->offset)
             ->limit($pages->limit)
