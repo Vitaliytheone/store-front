1ecab4bbffb4fdb3df0beb504349dcaf70565ce7
Alex Fatyeev <alex.fatyeev@gmail.com>
2018-05-28 14:38:55 +0300

    #ID-459 Small fixes.

--- my/modules/superadmin/views/child_panels/layouts/_panels_list.php
+++ my/modules/superadmin/views/child_panels/layouts/_panels_list.php
@@ -112,6 +112,9 @@
                                 'class' => 'dropdown-item',
                                 'target' => '_blank',
                             ])?>
+                            <?php if (Project::STATUS_FROZEN == $panel['act']): ?>
+                                <?= Html::a('Terminate panel', Url::toRoute(['/child-panels/change-status', 'id' => $panel['id'], 'status' => Project::STATUS_TERMINATED]), ['class' => 'dropdown-item'])?>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </td>
