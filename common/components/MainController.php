125bc652e8ff918d9a30204c770ae3a97c88e13e
vitaliysh <shulyakwork@gmail.com>
2018-06-20 15:51:21 +0300

    #ID-644 Removed debug code

--- common/components/MainController.php
+++ common/components/MainController.php
@@ -32,7 +32,7 @@ class MainController extends Controller
         // Validate post requests
         if (Yii::$app->request->isPost && $this->enableDomainValidation) {
             $refererDomain = !empty($_SERVER['HTTP_REFERER']) ? parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) : null;
-            if (empty($refererDomain) || mb_strtolower($refererDomain) === mb_strtolower($_SERVER['HTTP_HOST'])) {
+            if (empty($refererDomain) || mb_strtolower($refererDomain) !== mb_strtolower($_SERVER['HTTP_HOST'])) {
                 throw new ForbiddenHttpException(Yii::t('yii', 'Unable to verify your data submission.'));
             }
         }
