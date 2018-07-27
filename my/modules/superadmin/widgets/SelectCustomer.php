bf270c2c21c33a8507b4941e08a807fba0d14e5c
Андрей Глобин <globinav@Sommerces-iMac-2.local>
2018-07-27 11:16:53 +0300

    ID-727 fixes

--- my/modules/superadmin/widgets/SelectCustomer.php
+++ my/modules/superadmin/widgets/SelectCustomer.php
@@ -2,11 +2,11 @@
 
 namespace my\modules\superadmin\widgets;
 
+use common\models\panels\Customers;
 use yii\base\Widget;
 
 class SelectCustomer extends Widget
 {
-    public $models;
     public $context;
     public $name;
     public $selectedCustomerId = null;
@@ -18,8 +18,17 @@ class SelectCustomer extends Widget
      */
     public function run()
     {
+        $query  = Customers::find();
+        if ($this->status != null) {
+            $query->where([
+                'status' => $this->status
+            ]);
+        }
+
+        $models = $query->limit(10)->all();
+
         return $this->render('_select_customer', [
-            'models' => $this->models,
+            'models' => $models,
             'context' => $this->context,
             'status' => $this->status,
             'name' => $this->name,
