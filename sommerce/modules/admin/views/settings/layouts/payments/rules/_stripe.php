8805aabb001a759e8b1e6c37d2cd16e565a78220
Андрей Глобин <globinav@Sommerces-iMac-2.local>
2018-08-06 16:20:02 +0300

    stripe fix

---
+++ sommerce/modules/admin/views/settings/layouts/payments/rules/_stripe.php
@@ -0,0 +1,18 @@
+<?php
+    /** @var $store \common\models\stores\Stores */
+?>
+<ol>
+    <li><?= Yii::t('admin', 'settings.payments_stripe_guide_1', [
+            'signup_url' => '<a href="https://dashboard.stripe.com/account/apikeys" target="_blank">https://dashboard.stripe.com/account/apikeys</a>'
+        ]) ?>
+    </li>
+    <li>
+        <?= Yii::t('admin', 'settings.payments_stripe_guide_2', [
+            'url' => '<a href="https://dashboard.stripe.com/account/webhooks" target="_blank">https://dashboard.stripe.com/account/webhooks</a>'
+        ]) ?>
+        <?= Yii::t('admin', 'settings.payments_stripe_guide_3') ?>
+    </li>
+    <li>
+        <?= Yii::t('admin', 'settings.payments_stripe_guide_4') ?>
+    </li>
+</ol>
