059a1a6419d108255af42ae675cc248f52853daf
Vitaliy Shulyak <shulyakwork@gmail.com>
2018-09-13 00:30:19 +0300

    #ID-915 Removed super admin migrations

--- common/migrations/superadmin/20180801_db_panels_added_status_to_sender_log_table.sql
+++
@@ -1,4 +0,0 @@
-USE `panels`;
-
-ALTER TABLE `sender_log`
-ADD `status` tinyint(1) NULL COMMENT '1 - Success; 2 - Error; 3 - Curl error' AFTER `send_method`;
--- common/migrations/superadmin/20180829_referral_visits_add_index.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX idx_customer_id
-ON referral_visits (customer_id);
-
-ALTER TABLE `referral_visits` ADD CONSTRAINT `fk_referral_visits_customers` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
