fae0d72a11577004124040d205af2ab886d5396e
vitaliysh <shulyakwork@gmail.com>
2018-09-17 14:37:18 +0300

    Fix

--- my/modules/superadmin/views/statuses/sender.php
+++ my/modules/superadmin/views/statuses/sender.php
@@ -1,6 +1,7 @@
 <?php
 
 use my\components\ActiveForm;
+use my\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $datetime array */
@@ -15,7 +16,7 @@ use my\components\ActiveForm;
         <?php $form = ActiveForm::begin([
             'id' => 'createTicketForm',
             'method' => 'get',
-            'action' => '/superadmin/statuses/sender',
+            'action' => Url::toRoute('/statuses/sender'),
             'fieldConfig' => [
                 'template' => "{label}\n{input}",
                 'labelOptions' => ['class' => 'control-label'],
