d1ed2d183dc75677ff5462ac1a58fc7ce70abf9d
VitaliyZh <zhukovskiy.vitaliy@gmail.com>
2018-10-02 17:33:34 +0300

    #ID-1048 changed the view of editing page

--- sommerce/modules/admin/controllers/traits/settings/PagesTrait.php
+++ sommerce/modules/admin/controllers/traits/settings/PagesTrait.php
@@ -1,6 +1,7 @@
 <?php
 namespace sommerce\modules\admin\controllers\traits\settings;
 
+use Codeception\PHPUnit\Constraint\Page;
 use common\components\ActiveForm;
 use common\models\store\ActivityLog;
 use common\models\store\Pages;
@@ -8,6 +9,7 @@ use common\models\stores\StoreAdminAuth;
 use sommerce\controllers\CommonController;
 use sommerce\helpers\UiHelper;
 use sommerce\modules\admin\components\Url;
+use sommerce\modules\admin\models\forms\EditFilePageForm;
 use sommerce\modules\admin\models\forms\EditPageForm;
 use sommerce\modules\admin\models\forms\SavePageForm;
 use sommerce\modules\admin\models\search\PagesSearch;
@@ -93,7 +95,9 @@ trait PagesTrait {
             'pageId' => $pageForm->getPage()->id,
         ]);
 
-        return $this->render('edit_page', [
+        $view = $pageForm->getPage()->template == 'file' ? 'edit_page_file' : 'edit_page';
+
+        return $this->render($view, [
             'pageForm' => $pageForm,
             'isNewPage' => $pageForm->getPage()->isNewRecord,
             'storeUrl' => Yii::$app->store->getInstance()->getBaseSite(),
@@ -117,7 +121,13 @@ trait PagesTrait {
             exit;
         }
 
-        $pageForm = new EditPageForm();
+        $page = Pages::findOne($id);
+        if ($page->template == 'file') {
+            $pageForm = new EditFilePageForm();
+        } else {
+            $pageForm = new EditPageForm();
+        }
+
         $pageForm->setUser(Yii::$app->user);
 
         if (!$pageForm->edit($request->post(), $id)) {
