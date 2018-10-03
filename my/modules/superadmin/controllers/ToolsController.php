3254c1dd166b6c00e8ee554c9d0facb9980354bf
VitaliyZh <zhukovskiy.vitaliy@gmail.com>
2018-10-03 12:09:22 +0300

    #ID-988 updated the markup at Tools pages

--- my/modules/superadmin/controllers/ToolsController.php
+++ my/modules/superadmin/controllers/ToolsController.php
@@ -19,6 +19,8 @@ class ToolsController extends CustomController
     /** @var string Active navigation tab */
     public $activeTab = 'tools';
 
+    public $layout = 'superadmin_v2.php';
+
     /**
      * Renders the index view for the module
      * @return string
