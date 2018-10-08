0bfe5c5b18b3494afb8dcc1fed2aee3aed2db6b1
VitaliyZh <zhukovskiy.vitaliy@gmail.com>
2018-10-08 17:15:51 +0300

    #ID-988 fix

--- my/modules/superadmin/views/settings/payments.php
+++ my/modules/superadmin/views/settings/payments.php
@@ -12,7 +12,7 @@
         <div class="col-md-2">
             <div class="list-group list-group__custom">
                 <a href="<?=Url::toRoute('/settings')?>" class="list-group-item list-group-item-action active"><span class="fa fa-credit-card"></span> <?=Yii::t('app/superadmin', 'pages.settings.menu_payments')?></a>
-                <a href="<?=Url::toRoute('/settings/staff')?>" class="list-group-item list-group-item-action"><span class="fa fa-user-o"></span> <?=Yii::t('app/superadmin', 'pages.settings.menu_staff')?></a>
+                <a href="<?=Url::toRoute('/settings/staff')?>" class="list-group-item list-group-item-action"><span class="fa fa-user"></span> <?=Yii::t('app/superadmin', 'pages.settings.menu_staff')?></a>
                 <a href="<?=Url::toRoute('/settings/email')?>" class="list-group-item list-group-item-action"><span class="fa fa-envelope-o"></span> <?=Yii::t('app/superadmin', 'pages.settings.menu_email')?></a>
                 <a href="<?=Url::toRoute('/settings/plan')?>" class="list-group-item list-group-item-action"><span class="fa fa-list-alt"></span> <?=Yii::t('app/superadmin', 'pages.settings.menu_plan')?></a>
                 <a href="<?=Url::toRoute('/settings/content')?>" class="list-group-item list-group-item-action"><span class="fa fa-file-text-o"></span> <?=Yii::t('app/superadmin', 'pages.settings.content')?></a>
