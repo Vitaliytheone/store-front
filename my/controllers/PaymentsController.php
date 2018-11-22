d251cf0f9b55867ca4a58fccbfa36b1886e315db
vitaliysh <shulyakwork@gmail.com>
2018-11-22 16:34:21 +0300

    Removed unused code

--- my/controllers/PaymentsController.php
+++ my/controllers/PaymentsController.php
@@ -41,19 +41,6 @@ class PaymentsController extends CustomController
 
     }
 
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
-
     /**
      * @return \yii\web\Response
      * @throws \Throwable
