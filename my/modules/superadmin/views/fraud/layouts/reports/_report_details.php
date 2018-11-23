eb4b134c45331e890998792c3f66942ea0808222
VitaliyZh <zhukovskiy.vitaliy@gmail.com>
2018-11-23 16:30:39 +0300

    #ID-1171 fix

--- my/modules/superadmin/views/fraud/layouts/reports/_report_details.php
+++ my/modules/superadmin/views/fraud/layouts/reports/_report_details.php
@@ -4,9 +4,13 @@
     use my\helpers\SpecialCharsHelper;
 
     $detailsDecode = SpecialCharsHelper::multiPurifier($details->getDetails());
-
+?>
+<pre>
+    <?php
     if (isset($detailsDecode)) {
         print_r($detailsDecode);
     } else {
         print_r(SpecialCharsHelper::multiPurifier($details->transaction_details));
     }
+    ?>
+</pre>
