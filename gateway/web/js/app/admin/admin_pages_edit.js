f81e35f78663e6274e58097f9fbd86c9bb7d07b3
vitaliysh <shulyakwork@gmail.com>
2018-12-27 17:02:33 +0300

    #ID-1262 Updated edit pages

--- gateway/web/js/app/admin/admin_pages_edit.js
+++ gateway/web/js/app/admin/admin_pages_edit.js
@@ -29,6 +29,7 @@ customModule.adminPageEdit = {
         var isValidationUrlError = params.url_error || false;
 
         initSeoParts();
+        initSummernote($formFields.content);
 
         // Expand collapse if error
         if (isValidationUrlError) {
@@ -47,6 +48,36 @@ customModule.adminPageEdit = {
 
         // Start cleanup url
         $formFields.url.on('input', cleanupUrl);
+
+        /**
+         * Init summernote
+         * @param $element
+         */
+        function initSummernote($element){
+            if (!$formFields.content.length) {
+                return false;
+            }
+
+            $formFields.content.summernote({
+                minHeight: 300,
+                focus: true,
+                toolbar: [['style', ['style', 'bold', 'italic']], ['lists', ['ul', 'ol']], ['para', ['paragraph']], ['color', ['color']], ['insert', ['link', 'picture', 'video']], ['codeview', ['codeview']]],
+                disableDragAndDrop: true,
+                styleTags: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+                popover: {
+                    image: [['float', ['floatLeft', 'floatRight', 'floatNone']], ['remove', ['removeMedia']]]
+                },
+                dialogsFade: true
+            });
+
+            /* fix summernote submit form on Enter bug */
+            $pageForm.keypress(function (e) {
+                if (e.which === 13) {
+                    $pageForm.submit();
+                    return false;
+                }
+            });
+        }
 
         /**
          * Init autofill SEO part of the page
