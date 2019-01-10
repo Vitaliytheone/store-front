daf4417830068be487ce7c1513afeb1d19ee2b5b
Alex Fatyeev <alex.fatyeev@gmail.com>
2019-01-10 18:41:46 +0300

    1259 Base Stripe 3D Secure added.

---
+++ common/migrations/sommerce/20190109_added_stripe_3d_payment_method.sql
@@ -0,0 +1,3 @@
+USE `stores`;
+
+INSERT INTO `payment_gateways` (`id`, `method`, `currencies`, `name`, `class_name`, `url`, `position`, `options`, `visibility`) VALUES (NULL, 'stripe_3d_secure', '[\"RUB\", \"USD\"]', 'Stripe 3D Secure', 'Stripe3dSecure', 'stripe', '17', '{\"public_key\":\"\",\"secret_key\":\"\",\"webhook_secret\": \"\"}', '1');
