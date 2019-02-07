4749923608b2e39d238b82e0e7f0c986e08cd28a
VitaliyZh <zhukovskiy.vitaliy@gmail.com>
2019-02-07 12:31:16 +0300

    #ID-1327 fixed CORS for cross-domain requests

--- sommerce/components/filters/ApiAuthFilter.php
+++ sommerce/components/filters/ApiAuthFilter.php
@@ -50,6 +50,7 @@ class ApiAuthFilter extends ActionFilter
                     $admin = StoreAdminAuth::findOne(['status' => StoreAdminAuth::STATUS_ACTIVE]);
                 }
                 $user->setIdentity($admin);
+                header("Access-Control-Allow-Origin: *");
                 Yii::$app->controller->enableCsrfValidation = false;
                 Yii::$app->controller->enableDomainValidation = false;
             }
