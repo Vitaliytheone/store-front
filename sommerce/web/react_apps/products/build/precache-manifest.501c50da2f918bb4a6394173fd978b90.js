3849f682226e8600867e3d784724455452e7be21
Vitaliy <Vitaliythesecond@gmail.com>
2019-02-13 14:37:19 +0300

    build

---
+++ sommerce/web/react_apps/products/build/precache-manifest.501c50da2f918bb4a6394173fd978b90.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "e42f6e952df3cd6c679f",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "e42f6e952df3cd6c679f",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "2d5f15f92161454f1e3c4498d247f150",
+    "url": "/static/media/Metronic_fda1334c35d0f5fe2afb3afebbb6774a.2d5f15f9.eot"
+  },
+  {
+    "revision": "d751a5f71030efec99def8070dad7a81",
+    "url": "/static/media/Metronic_fda1334c35d0f5fe2afb3afebbb6774a.d751a5f7.woff2"
+  },
+  {
+    "revision": "b9c91f8037ec3d998566b33feb1f4a83",
+    "url": "/static/media/Metronic_fda1334c35d0f5fe2afb3afebbb6774a.b9c91f80.woff"
+  },
+  {
+    "revision": "f69ee90505162eb58ad8bf12da674b66",
+    "url": "/static/media/Metronic_fda1334c35d0f5fe2afb3afebbb6774a.f69ee905.ttf"
+  },
+  {
+    "revision": "6b6feb61813dddd403e663c2c307b51c",
+    "url": "/static/media/Metronic_fda1334c35d0f5fe2afb3afebbb6774a.6b6feb61.svg"
+  },
+  {
+    "revision": "608fd10666a860567740497e84492928",
+    "url": "/static/media/summernote.608fd106.woff"
+  },
+  {
+    "revision": "372a2830643e759ed61f6a8590900192",
+    "url": "/static/media/summernote.372a2830.eot"
+  },
+  {
+    "revision": "ce9ce49a0e1ca283fa242ba2cf8e24d9",
+    "url": "/static/media/summernote.ce9ce49a.ttf"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "452a5b42cb4819f09d35bcf6cbdb24c1",
+    "url": "/static/media/line-awesome.452a5b42.woff2"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "8b1290595e57e1d49d95ff3fa1129ecc",
+    "url": "/static/media/line-awesome.8b129059.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3f85d8035b4ccd91d2a1808dd22b7684",
+    "url": "/static/media/line-awesome.3f85d803.eot"
+  },
+  {
+    "revision": "4d42f5f0c62a8f51e876c14575354a6e",
+    "url": "/static/media/line-awesome.4d42f5f0.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "5207295c5bccd6e7442cfb261446c1f7",
+    "url": "/static/media/line-awesome.5207295c.svg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f9cf4225bebb48aaaecc56eb1b412569",
+    "url": "/index.html"
+  }
+];
