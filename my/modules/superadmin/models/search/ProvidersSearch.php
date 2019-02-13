3e77db89be4efcd9d5944e0e014371d8ca498d1c
MichaelK <zlocorp@gmail.com>
2019-02-13 17:27:06 +0300

    #ID-1403 My - Namesilo v1.11 (provider view fixes)

--- my/modules/superadmin/models/search/ProvidersSearch.php
+++ my/modules/superadmin/models/search/ProvidersSearch.php
@@ -3,6 +3,7 @@ namespace superadmin\models\search;
 
 use common\models\panels\AdditionalServices;
 use common\models\panels\Project;
+use my\helpers\DomainsHelper;
 use my\helpers\Url;
 use Yii;
 use yii\data\Sort;
@@ -240,7 +241,7 @@ class ProvidersSearch
                 'form_data' => $provider,
                 'id' => $provider['id'],
                 'provider_id' => $provider['provider_id'],
-                'name' => $provider['name'],
+                'name' => DomainsHelper::idnToUtf8($provider['name']),
                 'count' => $provider['service_count'],
                 'projects' => array_values($projects),
                 'in_use' => $provider['service_inuse_count'],
