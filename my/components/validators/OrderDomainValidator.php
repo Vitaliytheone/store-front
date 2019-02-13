eadfc068307c22406e3c891adc9e86d3e44f6340
MichaelK <zlocorp@gmail.com>
2019-02-13 14:33:01 +0300

    #ID-1403 My - Namesilo v1.11 (remove unused code)

--- my/components/validators/OrderDomainValidator.php
+++ my/components/validators/OrderDomainValidator.php
@@ -169,17 +169,6 @@ class OrderDomainValidator extends BaseDomainValidator
             return false;
         }
 
-        // uncomment to enable only (a-Z\d-) validation for all domains (including user domains)
-        /*
-        $registrar = DomainsHelper::getRegistrarClass($domain);
-        $result = $registrar::domainsCheck($domain);
-
-        if (empty($result[$domain])) {
-            $model->addError($attribute, Yii::t('app', 'error.panel.invalid_domain_name'));
-            return false;
-        }
-        */
-
         $existsDomain = Orders::find()->andWhere([
             'domain' => DomainsHelper::idnToAscii($domain),
             'item' => Orders::ITEM_BUY_DOMAIN,
