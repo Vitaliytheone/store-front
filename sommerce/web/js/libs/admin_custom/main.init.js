23dbe4ac477ace9d6f8f2b55db7bcccc58dd898a
Vitaliy Shulyak <shulyakwork@gmail.com>
2019-02-22 11:30:56 +0300

    Changes

--- sommerce/web/js/libs/admin_custom/main.init.js
+++ sommerce/web/js/libs/admin_custom/main.init.js
@@ -1,19 +1,18 @@
+'use strict';
+
 $('.dropdown-collapse').on('click', function (event) {
     event.preventDefault();
     event.stopPropagation();
-    if(($(this).next()).hasClass('show')){
+    if ($(this).next().hasClass('show')) {
         $($(this).attr('href')).collapse('hide');
-    }else{
+    } else {
         $($(this).attr('href')).collapse('show');
     }
 });
 
-
-
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
 });
-
 
 $(document).ready(function () {
     var inputs = document.querySelectorAll('.inputfile');
@@ -23,9 +22,9 @@ $(document).ready(function () {
 
         input.addEventListener('change', function (e) {
             var fileName = '';
-            if (this.files && this.files.length > 1){
-                fileName = ( this.getAttribute('data-multiple-caption') || '' ).replace('{count}', this.files.length);
-            }else {
+            if (this.files && this.files.length > 1) {
+                fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+            } else {
                 fileName = e.target.value.split('\\').pop();
             }
             if (fileName) {
@@ -35,69 +34,55 @@ $(document).ready(function () {
                     var reader = new FileReader();
 
                     reader.onload = function (e) {
-                        let template = `<div class="sommerce-settings__theme-imagepreview">
-                              <a href="#" class="sommerce-settings__delete-image"><span class="fa fa-times-circle-o"></span></a>
-                              <img src="${e.target.result}" alt="...">
-                          </div>`;
+                        var template = '<div class="sommerce-settings__theme-imagepreview">\n                              <a href="#" class="sommerce-settings__delete-image"><span class="fa fa-times-circle-o"></span></a>\n                              <img src="' + e.target.result + '" alt="...">\n                          </div>';
                         $('#image-preview').html(template);
                     };
                     reader.readAsDataURL(this.files[0]);
                 }
-            }else {
+            } else {
                 //label.innerHTML = labelVal;
             }
         });
-        $(document).on('click', '.sommerce_v1.0-settings__delete-image', function(e) {
+        $(document).on('click', '.sommerce_v1.0-settings__delete-image', function (e) {
             $('#image-preview').html('<span></span>');
             input.value = '';
         });
     });
-
 });
-
 
 /* Edit page */
 $(document).ready(function () {
 
-    if($('.edit-seo__title').length > 0) {
+    if ($('.edit-seo__title').length > 0) {
+        (function () {
 
-        let seoEdit = ['edit-seo__title', 'edit-seo__meta', 'edit-seo__url'];
+            var seoEdit = ['edit-seo__title', 'edit-seo__meta', 'edit-seo__url'];
 
-        for (let i = 0; i < seoEdit.length; i++) {
-            $("." + seoEdit[i] + '-muted').text($("#" + seoEdit[i]).val().length);
-            $("#" + seoEdit[i]).on('input', function (e) {
-                if (i == 2) {
-                    $('.' + seoEdit[i]).text($(e.target).val().replace(/\s+/g, '-'));
-                } else {
-                    $("." + seoEdit[i] + '-muted').text($(e.target).val().length);
-                    $('.' + seoEdit[i]).text($(e.target).val());
+            var _loop = function _loop(i) {
+                if ($("#" + seoEdit[i]).length) {
+                    $("." + seoEdit[i] + '-muted').text($("#" + seoEdit[i]).val().length);
                 }
-            });
-        }
+                $("#" + seoEdit[i]).on('input', function (e) {
+                    if (i == 2) {
+                        $('.' + seoEdit[i]).text($(e.target).val().replace(/\s+/g, '-'));
+                    } else {
+                        $("." + seoEdit[i] + '-muted').text($(e.target).val().length);
+                        $('.' + seoEdit[i]).text($(e.target).val());
+                    }
+                });
+            };
+
+            for (var i = 0; i < seoEdit.length; i++) {
+                _loop(i);
+            }
+        })();
     }
 });
-
 
 $(document).ready(function () {
     $('#select-menu-link').change(function () {
         $('.hide-link').hide();
-        let val = $("#select-menu-link option:selected").val();
-        $('.link-'+val).fadeIn()
+        var val = $("#select-menu-link option:selected").val();
+        $('.link-' + val).fadeIn();
     });
 });
-
-
-$(document).ready(function () {
-    $(document).on('click', '.delete-properies', function () {
-        $(this).parent().remove();
-    });
-    $(document).on('click', '.add-properies', function () {
-        let inputProperties = $('.input-properties').val();
-        if(inputProperties.length) {
-            $('.list-preperties').append(`<li class="list-group-item">${inputProperties} <span class="fa fa-times delete-properies"></span></li>`)
-            $('.input-properties').val('');
-        }
-    });
-});
-
-
