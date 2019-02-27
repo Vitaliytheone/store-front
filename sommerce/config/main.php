9cfaf99f3a040bd3be9e9d94b1e8faa3ddc193b0
MichaelK <zlocorp@gmail.com>
2019-02-27 10:56:54 +0300

    #ID-1507 Stores - Pages v1 (configs restore)

--- sommerce/config/main.php
+++ sommerce/config/main.php
@@ -170,9 +170,7 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => $params['debugIps'],
-        'historySize' => 500,
-        'traceLine' => '<a href="phpstorm://open?file={file}&line={line}">{file}:{line}</a>',
+        'allowedIPs' => $params['debugIps']
     ];
 
     $config['bootstrap'][] = 'gii';
