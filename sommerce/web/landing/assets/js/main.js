c45f17ffbc56cf6a0c0eea0049eec2cc8117046b
Vitaliy <Vitaliythesecond@gmail.com>
2019-02-28 19:08:09 +0300

    desktop full

---
+++ sommerce/web/landing/assets/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  //Scrolling
+  $(document).ready(function() {
+    $("#navbar-scrolling").on("click", "a", function(event) {
+      var navbarHeight = $("nav").height();
+      event.preventDefault();
+      var id = $(this).attr("href"),
+        top = $(id).offset().top;
+      var scrollTopHeigt = top - navbarHeight;
+      $("body,html").animate({ scrollTop: scrollTopHeigt }, 1000);
+    });
+  });
+
+  $(document).ready(function() {
+    $("#footer-scrolling").on("click", "a", function(event) {
+      var footerHeight = $("nav").height();
+      event.preventDefault();
+      var id = $(this).attr("href"),
+        top = $(id).offset().top;
+      var scrollTopHeigt = top - footerHeight;
+      $("body,html").animate({ scrollTop: scrollTopHeigt }, 1000);
+    });
+  });
+
+  
+});
