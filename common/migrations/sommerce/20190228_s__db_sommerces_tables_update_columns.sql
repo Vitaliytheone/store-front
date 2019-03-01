2e386034efe7355df57dd663319ad8eaad1ef076
MichaelK <zlocorp@gmail.com>
2019-03-01 10:24:52 +0300

    #ID-1477 Sommerce - Delete unused (merge and delete migrations)

--- common/migrations/sommerce/20190228_s__db_sommerces_tables_update_columns.sql
+++
@@ -1,9 +0,0 @@
-#
-# TODO:: Only for new SOMMERCE branch!
-#
-
-USE `sommerce_template`;
-
-ALTER TABLE `suborders` CHANGE `amount` `amount` DECIMAL(20,2) NULL DEFAULT NULL;
-ALTER TABLE `payments` CHANGE `amount` `amount` DECIMAL(20,2) NULL DEFAULT NULL;
-ALTER TABLE `payments` CHANGE `fee` `fee` DECIMAL(20,2) NULL DEFAULT '0.00';
