1f4e1f257c2c7ceca27bcfc0769e9dfc9dc2c6c6
Vitaliy <Vitaliythesecond@gmail.com>
2019-03-04 00:02:15 +0300

    landing end v1

--- sommerce/web/landing/assets/js/main.js
+++ sommerce/web/landing/assets/js/main.js
@@ -1,48 +1,42 @@
 $(document).ready(function() {
-  //Scrolling
-  $(document).ready(function() {
-    $("#navbar-scrolling").on("click", "a", function(event) {
-      var navbarHeight = $("nav").height();
-      event.preventDefault();
-      var id = $(this).attr("href"),
-        top = $(id).offset().top;
-      var scrollTopHeigt = top - navbarHeight;
-      $("body,html").animate({ scrollTop: scrollTopHeigt }, 1000);
-    });
-  });
+	//Scrolling
+	$(document).ready(function() {
+		$('#navbar-scrolling').on('click', 'a', function(event) {
+			var navbarHeight = $('nav').height();
+			event.preventDefault();
+			var id = $(this).attr('href'),
+				top = $(id).offset().top;
+			var scrollTopHeigt = top - navbarHeight;
+			$('body,html').animate({ scrollTop: scrollTopHeigt }, 1000);
+		});
+	});
 
-  $(document).ready(function() {
-    $("#footer-scrolling").on("click", "a", function(event) {
-      var footerHeight = $("nav").height();
-      event.preventDefault();
-      var id = $(this).attr("href"),
-        top = $(id).offset().top;
-      var scrollTopHeigt = top - footerHeight;
-      $("body,html").animate({ scrollTop: scrollTopHeigt }, 1000);
-    });
-  });
+	$(document).ready(function() {
+		$('#footer-scrolling').on('click', 'a', function(event) {
+			var footerHeight = $('nav').height();
+			event.preventDefault();
+			var id = $(this).attr('href'),
+				top = $(id).offset().top;
+			var scrollTopHeigt = top - footerHeight;
+			$('body,html').animate({ scrollTop: scrollTopHeigt }, 1000);
+		});
+	});
 
-  // function slickify() {
-  //   $('.slick').slick({
-  //     autoplay: true,
-  //     autoplaySpeed: 4000,
-  //     delay: 5000,
-  //     speed: 700,
-  //     responsive: [
-  //       {
-  //         breakpoint: 500,
-  //         settings: "unslick",
-  //       }
-  //     ]
-  //   });
-  // }
+	// function slickify() {
+	// 	$('.slick').slick({
+	// 		autoplay: false,
+	// 		delay: 5000,
+	// 		speed: 700
+	// 	});
+	// }
 
-  // slickify();
-  // $(window).resize(function () {
-  //   var $windowWidth = $(window).width();
-  //   if ($windowWidth < 500) {
-  //     slickify();
-  //   }
-  // });
-  
+	// slickify();
+
+	// $(window).on('resize', () => {
+	// 	if (document.body.clientWidth > 576) {
+	// 		$('.slick').slick('unslick');
+	// 	} else {
+	// 		slickify();
+	// 	}
+	// });
 });
