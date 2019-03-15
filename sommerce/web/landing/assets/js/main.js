fd0343bafd17954126262ca498ecb129b74d48e4
Vitaliy <Vitaliythesecond@gmail.com>
2019-03-15 19:12:19 +0300

    Scroll new

--- sommerce/web/landing/assets/js/main.js
+++ sommerce/web/landing/assets/js/main.js
@@ -11,16 +11,7 @@ $(document).ready(function () {
 		});
 	});
 
-	// $(document).ready(function () {
-	// 	$('#pricing').on('click', 'a', function (event) {
-	// 		var navbarHeight = $('nav').height();
-	// 		event.preventDefault();
-	// 		var id = $(this).attr('href'),
-	// 			top = $(id).offset().top;
-	// 		var scrollTopHeigt = top - navbarHeight;
-	// 		$('body,html').animate({ scrollTop: scrollTopHeigt }, 1000);
-	// 	});
-	// });
+
 	$(".navbar-link").click(function (event) {
 		event.preventDefault();
 	});
@@ -28,14 +19,15 @@ $(document).ready(function () {
 	// $(function () {
 	// 	$('[data-toggle="popover"]').popover()
 	// })
-	$(document).ready(function () {
-		if ($(window).scrollTop) {
-			$(".navbar").toggleClass("nav-scroll");
-		}
-	});
+
+	// $(document).ready(function () {
+	// 	if ($(window).scrollTop) {
+	// 		$(".navbar").toggleClass("nav-scroll");
+	// 	}
+	// });
 
 	$(window).scroll(function () {
-		$(".navbar").toggleClass("nav-scroll", $(this).scrollTop() > 0);
+		$(".navbar").toggleClass("nav-shadow", $(this).scrollTop() > 0);
 	});
 
 });
