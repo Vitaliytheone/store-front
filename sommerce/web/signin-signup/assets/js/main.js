52acc0c816018b4265cbd21c16805fb384c768d6
Vitaliy <Vitaliythesecond@gmail.com>
2019-03-22 15:07:33 +0300

    invoice

--- sommerce/web/signin-signup/assets/js/main.js
+++ sommerce/web/signin-signup/assets/js/main.js
@@ -1,8 +1,32 @@
-// //set button id on click to hide first modal
-// $("#signin").on("click", function () {
-//     $('#myModal1').modal('hide');
-// });
-// //trigger next modal
-// $("#signin").on("click", function () {
-//     $('#myModal2').modal('show');
-// });
+// Input type file;
+
+var inputs = document.querySelectorAll('.hide');
+Array.prototype.forEach.call(inputs, function (input) {
+    var label = input.nextElementSibling,
+        labelVal = label.innerHTML;
+    input.addEventListener('change', function (e) {
+        var fileName = '';
+        if (this.files && this.files.length > 1)
+            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+        else
+            var a  = e.target.value.split('\\').pop();
+
+
+        var split = a.split('.');
+        console.log(split);
+        var fileNames = split[0];
+        var extension = split[1];
+    
+
+        if (fileNames.length > 10) {
+            fileNames = fileNames.substring(0, 10);
+        }
+
+         fileName = fileNames + '.' + extension;
+
+        if (fileName)
+            label.querySelector('span').innerHTML = fileName;
+        else
+            label.innerHTML = labelVal;
+    });
+});
