bc45e67776da85c0a8563eb0e4e8615fec5559eb
Vitaliy <Vitaliythesecond@gmail.com>
2019-03-23 10:06:50 +0300

    Attach file in support modal

--- sommerce/web/signin-signup/assets/js/main.js
+++ sommerce/web/signin-signup/assets/js/main.js
@@ -1,32 +1,43 @@
 // Input type file;
 
-var inputs = document.querySelectorAll('.hide');
-Array.prototype.forEach.call(inputs, function (input) {
-    var label = input.nextElementSibling,
-        labelVal = label.innerHTML;
-    input.addEventListener('change', function (e) {
-        var fileName = '';
-        if (this.files && this.files.length > 1)
-            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-        else
-            var a  = e.target.value.split('\\').pop();
+var inputs = document.querySelectorAll(".hide");
+Array.prototype.forEach.call(inputs, function(input) {
+  var label = input.nextElementSibling,
+    labelVal = label.innerHTML;
+  input.addEventListener("change", function(e) {
+    var fileName = "";
+    if (this.files && this.files.length > 1)
+      fileName = (this.getAttribute("data-multiple-caption") || "").replace(
+        "{count}",
+        this.files.length
+      );
+    else {
+      var fullFileName = e.target.value.split("\\").pop();
 
+      if (fullFileName != "") {
+        var fileNameSplit = fullFileName.split(".");
+        var firstName = fileNameSplit[0];
+        var extension = fileNameSplit[1];
 
-        var split = a.split('.');
-        console.log(split);
-        var fileNames = split[0];
-        var extension = split[1];
-    
-
-        if (fileNames.length > 10) {
-            fileNames = fileNames.substring(0, 10);
+        if (firstName.length > 10) {
+          firstName = firstName.substring(0, 10);
         }
 
-         fileName = fileNames + '.' + extension;
+        fileName = firstName + "." + extension;
+      }
+    }
 
-        if (fileName)
-            label.querySelector('span').innerHTML = fileName;
-        else
-            label.innerHTML = labelVal;
-    });
+    if (fileName) {
+      console.log(fileName);
+      label.querySelector("span").innerHTML = fileName;
+      label.querySelector("button").classList.add("new-privet");
+      var button = label.querySelector("button");
+      button.addEventListener("click", function(event) {
+        document.getElementById("attach-file").value = null;
+        console.log(fileName);
+        label.innerHTML = labelVal;
+        event.preventDefault();
+      });
+    } else label.innerHTML = labelVal;
+  });
 });
